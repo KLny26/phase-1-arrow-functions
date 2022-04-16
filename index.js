43f0ca75eaf47108bfb2f04b31e1c001bd3b3d63
kristenli <kyli2005@gmail.com>
2022-04-16 18:17:15 -0400

    commit tree 0532480db954fba54435062af9199234715326b4

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1 = 2000
+let num2 = 100
+const divide = function (){
+    return(num1 / num2)
+}
+divide();
+
+const square = (x) => x * x ;
+square();
+
+const add = (x,y) => x + y;
+add(x, y)
